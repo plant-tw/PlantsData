a86a358bc629dd4a704c01ba8233f6df897c063d
denkeni <denkenie@gmail.com>
2018-08-21 11:46:16 +0800

    Refactor with JSLint

--- docs/index.js
+++ docs/index.js
@@ -1,55 +1,62 @@
-var doc = function() {
-  var obj;
-  var show = function(key) {
-    if (typeof(key) !== "string") {
-      document.getElementsByClassName("name")[0].textContent = "Error: not string type";
-      document.getElementsByClassName("description")[0].textContent = "";
-      document.getElementsByClassName("image")[0].src = "";
-      return;
-    }
-    loadJSON(function(response) {
-      var data = JSON.parse(response);
-      obj = data[key];
-      if (obj === undefined) {
-        document.getElementsByClassName("name")[0].textContent = "Error: not found";
-        document.getElementsByClassName("description")[0].textContent = "";
-        document.getElementsByClassName("image")[0].src = "";
-        return;
-      }
-      var name = key;
-      var txt = obj.txt;
-      document.getElementsByClassName("name")[0].textContent = name;
-      document.getElementsByClassName("description")[0].textContent = txt;
-      document.getElementsByClassName("image")[0].src = "";
-    });
-  };
+var doc = (function () {
+    "use strict";
+    var obj;
 
-  var loadImages = function() {
-    if (obj === undefined) {
-      document.getElementsByClassName("name")[0].textContent = "Error: not found";
-      document.getElementsByClassName("description")[0].textContent = "";
-      document.getElementsByClassName("image")[0].src = "";
-      return;
-    }
-    document.getElementsByClassName("image")[0].src = obj.img;
-  };
-
-  // See: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-  var loadJSON = function(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open("GET", "data.json", true);
-    xobj.onreadystatechange = function () {
-     if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
+    // See: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+    var loadJSON = function (callback) {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open("GET", "data.json", true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState === 4 && xobj.status === "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
     };
-    xobj.send(null);
- };
 
-  return {
-    loadImages: loadImages,
-    show: show
-  };
-}();
+    var show = function (key) {
+        var errMsg = "";
+        if (typeof(key) !== "string") {
+            errMsg = "Error: not string type";
+            document.getElementsByClassName("name")[0].textContent = errMsg;
+            document.getElementsByClassName("description")[0].textContent = "";
+            document.getElementsByClassName("image")[0].src = "";
+            return;
+        }
+        loadJSON(function (response) {
+            var data = JSON.parse(response);
+            obj = data[key];
+            if (obj === undefined) {
+                errMsg = "Error: not found";
+                document.getElementsByClassName("name")[0].textContent = errMsg;
+                document.getElementsByClassName("description")[0].textContent = "";
+                document.getElementsByClassName("image")[0].src = "";
+                return;
+            }
+            var name = key;
+            var txt = obj.txt;
+            document.getElementsByClassName("name")[0].textContent = name;
+            document.getElementsByClassName("description")[0].textContent = txt;
+            document.getElementsByClassName("image")[0].src = "";
+        });
+    };
+
+    var loadImages = function () {
+        var errMsg = "";
+        if (obj === undefined) {
+            errMsg = "Error: run show() before loadImages()";
+            document.getElementsByClassName("name")[0].textContent = errMsg;
+            document.getElementsByClassName("description")[0].textContent = "";
+            document.getElementsByClassName("image")[0].src = "";
+            return;
+        }
+        document.getElementsByClassName("image")[0].src = obj.img;
+    };
+
+    return {
+        loadImages: loadImages,
+        show: show
+    };
+}());
